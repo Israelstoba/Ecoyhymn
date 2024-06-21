6197782eb9291b619bfff01dbf234a70febb8371
Israel Stoba <israelstoba@gmail.com>
2024-06-21 03:16:37 +0100

    Added the search element

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ mainCon.addEventListener('click', function () {
 homeIcon.addEventListener('click', function () {
   appCon.style.left = '100%';
 });
+
+////search element//////////
+var searchBtn = document.querySelector('#search-btn');
+var searchCloseBtn = document.querySelector('#search-el-close');
+var searchWrapper = document.querySelector('#search-wrapper');
+var searchEl = document.querySelector('#search-el');
+
+searchBtn.addEventListener('click', function () {
+  searchWrapper.style.left = '0';
+});
+
+searchCloseBtn.addEventListener('click', function () {
+  searchWrapper.style.left = '100%';
+  searchEl.value = '';
+});
